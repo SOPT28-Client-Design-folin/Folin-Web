8186297ca8f5dbc84a9a04b9b50a1b4ac881d92f
Hyoin-Kim <hi980506@naver.com>
2021-05-22 14:22:58 +0900

    [feat] 라우팅

--- folin-web/src/App.js
+++ folin-web/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Router from "./components/Router";
 import GlobalStyles from "./assets/styles/GlobalStyles";
 import theme from "./assets/styles/theme";
+import Second from '../src/components/Second/Second.js';
 import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router />
+      <Router path="/second">
+        <Second />
+        </Router>
       <GlobalStyles />
     </ThemeProvider>
   );
