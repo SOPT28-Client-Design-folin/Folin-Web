a2bbdfb8c366ba87d24dcf4b38bc96e9795f93f5
YeongWoooo <rdd9223@gmail.com>
2021-05-28 16:43:43 +0900

    [fix] first div z-index 뒤로가지던 버그 수정

--- folin-web/src/components/First/First.js
+++ folin-web/src/components/First/First.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Styled from "styled-components";
-import Slider from "./Up/Slider/Slider";
-import Story from "../First/Up/Story";
 import Keyword from "../First/Up/Keyword";
+import Story from "../First/Up/Story";
 import Down from "./Down";
+import Slider from "./Up/Slider/Slider";
 
 const data = [
   {
@@ -103,7 +103,6 @@ const TopWrapper = Styled.div`
     top: 20vh;
     width: 100%;
     display: flex;
-    z-index: -1;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
