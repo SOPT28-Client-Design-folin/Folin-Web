d1680b73b04d4a807887846f40c18e20eed81d7f
YeongWoooo <rdd9223@gmail.com>
2021-05-29 18:34:59 +0900

    [style] 코드포멧터 옵션 변경으로 인한 코드 스타일 변경

--- folin-web/src/components/First/First.js
+++ folin-web/src/components/First/First.js
@@ -1,100 +1,100 @@
-import React from "react";
-import Styled from "styled-components";
-import Keyword from "../First/Up/Keyword";
-import Story from "../First/Up/Story";
-import Down from "./Down";
-import Slider from "./Up/Slider/Slider";
+import React from 'react';
+import Styled from 'styled-components';
+import Keyword from '../First/Up/Keyword';
+import Story from '../First/Up/Story';
+import Down from './Down';
+import Slider from './Up/Slider/Slider';
 
 const data = [
   {
     id: 1,
     rank: 1,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 2,
     rank: 2,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 3,
     rank: 3,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 4,
     rank: 4,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 5,
     rank: 5,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 6,
     rank: 6,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 7,
     rank: 7,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 8,
     rank: 8,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 9,
     rank: 9,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
   {
     id: 10,
     rank: 10,
-    img: "https://via.placeholder.com/200",
-    categories: ["비즈니스", "라이프 스타일"],
+    img: 'https://via.placeholder.com/200',
+    categories: ['비즈니스', '라이프 스타일'],
     title: "'힙한'브랜드 모베러웍스 만든 세번의 실패",
-    publisher: "모빌스 그룹",
-    status: "무료",
+    publisher: '모빌스 그룹',
+    status: '무료',
   },
 ];
 
@@ -106,7 +106,7 @@ const TopWrapper = Styled.div`
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    font-family: "Nanum Myeongjo";
+    font-family: "Noto Serif KR";
     font-weight: 500;
 `;
 
