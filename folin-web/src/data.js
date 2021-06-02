2b7bee4ed992f0aea093bf1f801ac0348b1afeb6
Hyoin-Kim <hi980506@naver.com>
2021-06-02 15:45:56 +0900

    [update] Middle.js 수정 및 data.js 추가, feat/9 merge

---
+++ folin-web/src/data.js
@@ -0,0 +1,81 @@
+export default [
+    {
+      id : 0,
+      title : "데이터사이언스, 숫자를 전략으로 바꾸다.",
+      author : "차현나",
+      publisher : "하이브(빅히트엔터테인먼트)데이터랩장",
+      general_price : " 일반 회원가 30,000원",
+      membership_price : " 멤버십 회원가 0원",
+      type: "신청하기"
+    },
+  
+    {
+      id : 1,
+      title : "뤼이드 어떻게 세계 100대 AI 기업이 되었나",
+      author : "임정현",
+      publisher : "뤼이드부사장",
+      general_price : " 일반 회원가 30,000원",
+      membership_price : " 멤버십 회원가 0원",
+      type: "신청하기"
+    },
+  
+    {
+      id : 2,
+      title : "끝없는 성장, '콘텐츠 온리' 시대가 온다",
+      author : "노가영",
+      publisher : "IT미디어 트렌드북 작가",
+      general_price : " 일반 회원가 30,000원",
+      membership_price : " 멤버십 회원가 0원",
+      type: "신청하기"
+    },
+
+    {
+        id : 3,
+        title : "MZ, 백화점대신 앱에서 명품 사는 이유",
+        author : "최형록",
+        publisher : "발란 대표",
+        general_price : " ",
+        membership_price : " ",
+        type: "모임완료"
+      },
+
+      {
+        id : 4,
+        title : "회사 대신 '직업'을 바꾸는 법",
+        author : "김나이",
+        publisher : "커리어 액셀러레이터",
+        general_price : "",
+        membership_price : "",
+        type: "모임완료"
+      },
+
+      {
+        id : 5,
+        title : "오프라인 비즈니스, 상상력이 미래를 바꾼다",
+        author : "이동진 정종현 김소영",
+        publisher : "",
+        general_price : "",
+        membership_price : "",
+        type: "모임완료"
+      },
+
+      {
+        id : 6,
+        title : "50살 삼립호빵, MZ와 노는 브랜드된 비결",
+        author : "조준형",
+        publisher : "SPC삼립 브린드 커뮤니케이션 팀장",
+        general_price : "",
+        membership_price : "",
+        type: "모임완료"
+      },
+
+      {
+        id : 7,
+        title : "카카오와 네이버는 어떻게 MZ의 은행이 됐나",
+        author : "김강원",
+        publisher : "베인앤드컴퍼니 경영 컨설턴트",
+        general_price : "",
+        membership_price : "",
+        type: "모임완료"
+      }
+  ] 
